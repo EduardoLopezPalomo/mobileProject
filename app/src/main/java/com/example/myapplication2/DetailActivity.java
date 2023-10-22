94abdfc6a768f03607198350bf032292841aabbf
Eduardo <e.lpalomo@alumnos.upm.es>
2023-10-22 18:53:44 +0300

    buyers and info

--- app/src/main/java/com/example/myapplication2/DetailActivity.java
+++ app/src/main/java/com/example/myapplication2/DetailActivity.java
@@ -7,7 +7,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -21,16 +25,44 @@ public class DetailActivity extends AppCompatActivity {
         String item = in.getStringExtra("com.example.myapplication2.Item");
         String price = in.getStringExtra("com.example.myapplication2.Price");
         String desc = in.getStringExtra("com.example.myapplication2.Desc");
+
         int pic = getImg(item);
         ImageView img = findViewById(R.id.imageView);
         TextView itemText = findViewById(R.id.itemTextView);
         TextView priceText = findViewById(R.id.costTextView);
         TextView descText = findViewById(R.id.descriptionText);
+        TextView buyer = findViewById(R.id.buyerTextView);
+        TextView infoText = findViewById(R.id.infoTextView);
+        Switch info = findViewById(R.id.seeInfoSwitch);
+        TextView valoration = findViewById(R.id.ratingTextView);
+        RatingBar rating = findViewById(R.id.buyerRatingBar);
+
+        buyer.append(getBuyer(item));
+        infoText.setText("name: " + getBuyer(item)+ "\nDate of birth: 14/11/1995\nGender: Male\nLocation: Earth");
+        valoration.setText("valoration:");
+        infoText.setVisibility(View.INVISIBLE);
+        valoration.setVisibility(View.INVISIBLE);
+        rating.setVisibility(View.INVISIBLE);
 
         itemText.setText(item);
         priceText.setText(price);
         descText.setText(desc);
         scaleImg(img,pic);
+
+        info.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    infoText.setVisibility(View.VISIBLE);
+                    valoration.setVisibility(View.VISIBLE);
+                    rating.setVisibility(View.VISIBLE);
+                }else{
+                    infoText.setVisibility(View.INVISIBLE);
+                    valoration.setVisibility(View.INVISIBLE);
+                    rating.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
     private int getImg(String item){
@@ -40,6 +72,15 @@ public class DetailActivity extends AppCompatActivity {
             case "apple": return R.drawable.apple;
             case "banana": return R.drawable.banana;
             default: return -1;
+        }
+    }
+    private String getBuyer(String item){
+        switch (item){
+            case "peach": return "Kanan";
+            case "tomato": return "Kobe";
+            case "apple": return "Mario";
+            case "banana": return "Aragorn";
+            default: return "";
         }
     }
     private void scaleImg(ImageView img, int pic){
